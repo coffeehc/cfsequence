6de4d91cf4dfb85ca6414597b3822974df777915
黄川 <huangchuan@51zhangdan.com.cn>
2015-12-04 10:44:24 +0800

    move file

--- main/main.go
+++ main/main.go
@@ -5,18 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"github.com/coffeehc/logger"
-	"github.com/coffeehc/utils"
 	"github.com/coffeehc/web"
+	"github.com/coffeehc/utils"
 )
 
-type service struct {
-	webServer       *web.Server
-	sequenceService *SequenceService
-}
 
 var (
-	nodeId    = flag.Int("nodeid", 0, "节点编号,最大255")
-	http_ip   = flag.String("http_ip", "0.0.0.0", "服务器地址")
+	nodeId = flag.Int("nodeid", 0, "节点编号,最大255")
+	http_ip = flag.String("http_ip", "0.0.0.0", "服务器地址")
 	http_port = flag.Int("http_port", 8888, "服务提供地址")
 )
 
@@ -29,26 +25,19 @@ func main() {
 		fmt.Errorf("节点为0-255之间的值,请重新设置")
 		return
 	}
-	_service := newService()
-	args := make([]string, 3)
-	args[0] = fmt.Sprintf("-nodeid=%d", *nodeId)
-	args[1] = fmt.Sprintf("-http_ip=%s", *http_ip)
-	args[2] = fmt.Sprintf("-")
-	utils.ProcessServiceWithFlag(utils.CreatService("cfsequence", "cfsequence.service", "coffee's a sequence service", args, _service.start, _service.stop))
+	var webServer *web.Server
+	var sequenceService *SequenceService
+	service := utils.NewService(func() error{
+		webServer = web.NewServer(&web.ServerConfig{Addr: *http_ip, Port: *http_port})
+		sequenceService = newSequenceService(*nodeId)
+		sequenceService.regeditRestService(webServer)
+		return webServer.Start()
+	}  , func()error{
+		logger.Debug("webServer is %#v",webServer)
+		webServer.Stop()
+		return nil
+	})
+	utils.StartService(service)
 }
 
-func newService() *service {
-	_service := new(service)
-	_service.webServer = web.NewServer(&web.ServerConfig{Addr: *http_ip, Port: *http_port})
-	_service.sequenceService = newSequenceService(*nodeId)
-	_service.sequenceService.regeditRestService(_service.webServer)
-	return _service
-}
 
-func (this *service) start() {
-	this.webServer.Start()
-}
-
-func (this *service) stop() {
-	this.webServer.Stop()
-}
